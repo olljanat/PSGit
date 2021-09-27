4d6f75c1ddb115eaeeb1dad948b879ea5b5652fa
Olli Janatuinen <olljanat@users.noreply.github.com>
2021-09-27 06:18:15 -0700

    CI: Install depended modules before running tests (#1)
    CI: First working version
    
    * Include custom version of AzurePipelinesPS with Linux support
    * Do not install AzurePipelinesPS but instead of import custom version

--- _TestAll.ps1
+++ _TestAll.ps1
@@ -1,11 +1,13 @@
-# $AzDevToken = Read-Host -AsSecureString -Prompt "Give Azure DevOps PAT"
-# $GitHubToken = Read-Host -AsSecureString -Prompt "Give GitHub PAT"
+param (
+    [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string]$PlainTextPAT
+)
 
+
+$AzDevToken = ConvertTo-SecureString -AsPlainText $PlainTextPAT -Force
 import-module .\PSGit.psm1 -Force
 
 Write-Output "Testing Azure DevOps"
 Invoke-PSGitLogin -Platform "AzureDevOps" -Project "PSGitLab" -GitRepo "PSGitLab" -SecureToken $AzDevToken -Uri "https://dev.azure.com/olljanat"
-Get-PSGitRepo
 $env = New-PSGitEnvironment -Name "test" -Description "test environment"
 Get-PSGitEnvironment
 Remove-PSGitEnvironment -EnvironmentId "$($env.id)"
