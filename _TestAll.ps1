c8b48f23dbd07721daff6ecb422cab28f4bc9213
Olli Janatuinen <olli.janatuinen@aditro.com>
2021-09-27 14:22:14 +0300

    Add Azure DevOps PR validation

---
+++ _TestAll.ps1
@@ -0,0 +1,22 @@
+# $AzDevToken = Read-Host -AsSecureString -Prompt "Give Azure DevOps PAT"
+# $GitHubToken = Read-Host -AsSecureString -Prompt "Give GitHub PAT"
+
+import-module .\PSGit.psm1 -Force
+
+Write-Output "Testing Azure DevOps"
+Invoke-PSGitLogin -Platform "AzureDevOps" -Project "PSGitLab" -GitRepo "PSGitLab" -SecureToken $AzDevToken -Uri "https://dev.azure.com/olljanat"
+Get-PSGitRepo
+$env = New-PSGitEnvironment -Name "test" -Description "test environment"
+Get-PSGitEnvironment
+Remove-PSGitEnvironment -EnvironmentId "$($env.id)"
+Get-PSGitPullRequest
+
+# Write-Output "`r`n--------------`r`n"
+
+# Write-Output "Testing GitHub"
+# Invoke-PSGitLogin -Platform "GitHub" -Project "PSGitLab" -GitRepo "PSGitLab" -SecureToken $GitHubToken -Uri "https://github.com"
+# Get-PSGitRepo
+# $env = New-PSGitEnvironment -Name "test" -Description "test environment"
+# Get-PSGitEnvironment
+# Remove-PSGitEnvironment -EnvironmentId "-"
+# Get-PSGitPullRequest
